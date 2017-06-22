8859ca347744300a78984cee8febc3da24d6b032
Victor Luchits <vluchits@gmail.com>
2017-06-22 20:33:58 +0300

    Change the etcd module around to work with nginx

--- README.md
+++ README.md
@@ -1,60 +1,25 @@
-# tarantool-etcd
-Tarantool replication setup helper
+# nginx-etcd
+ETCD module for nginx
 
 ## Requirements
-* [tarantool](https://github.com/tarantool/tarantool)
-* [tarantool-http](https://github.com/tarantool/http)
+* [lua-nginx-module](https://github.com/openresty/lua-nginx-module)
+* [lua-resty-http](https://github.com/pintsized/lua-resty-http)
 
 ## Configuration
-Sample cluster instance configuration
+
 ```lua
--- reload package
-package.loaded['etcd'] = nil
-local etcd = require('etcd')
+server {
+        ...
 
--- kill all fibers with name starts with "etcd:"
--- required for reload configuration without restart
-etcd.killall()
- 
--- init etcd client with login and password
-local client = etcd.client("tarantool:****@10.255.6.110:4001")
+        lua_shared_dict etcd 32k;
 
--- init cluster object
-local cluster = client:cluster(
-    "/tarantool/cluster/mega_cluster", -- root directory in etcd for this cluster
-    "192.168.0.145:2641",  -- this instance host/port
-    {
-        role = "master",
-        replica_user = "replica",
-        replica_password = "qwerty",
-        dump = "/var/lib/tarantool/tarantool65_cluster.json",
-        discovery_ttl = 60,
-        discovery_refresh = 10,
-    }
-)
+        init_worker_by_lua '
+             local etcd = require("etcd")
+             local client = etcd.client("foo:bar@127.0.0.1:2379")
+             local opts -- ttl, refresh, timeout
+             client:announce("http/servers/127.0.0.1:80", "{\"weight\":2, \"max_fails\":2, \"fail_timeout\":10, \"down\":1}", ngx.shared.etcd, opts)
+        ';
 
--- detect first start
-if not etcd.file_exists("/snaps65/*.snap") then
-    cluster:bootstrap()
-end
- 
-box.cfg{
-    listen = 2641,
-    slab_alloc_arena = 0.2,
-    slab_alloc_factor = 1.04,
-    replication_source = cluster:replication_source(),
-    wal_dir    = "/data/xlogs65",
-    snap_dir   = "/snaps65",
-    logger     = "/var/log/tarantool/tarantool65.log",
-    pid_file   = "/var/run/tarantool/tarantool65.pid",
+        ...
 }
- 
--- user for replication
-box.once('user:replica:v1', function()
-    box.schema.user.create('replica', {password = 'qwerty'})
-    box.schema.user.grant('replica', 'read,write,execute', 'universe')
-end)
- 
--- run background etcd announce and replication_source refresh
-cluster:run()
 ```
--- etcd.lua
+++ etcd.lua
@@ -1,114 +1,70 @@
 module("etcd", package.seeall)
 
-local fiber = require('fiber')
-local http_client = require('http.client')
-local log = require('log')
-local urilib = require('uri')
-local fio = require('fio')
-local json = require('json')
-local clock = require('clock')
-local uuid = require('uuid')
-local digest = require('digest')
+local ngx = ngx
+local math = math
+local http = require("resty.http")
+local json = require("cjson")
 
-local function debugf(fmt, ...)
-    local c = debug.getinfo(2)
-    log.debug("[etcd.lua:%d:%s] "..fmt, c.currentline, c.name, ...)
+function debugf(fmt, ...)
+    ngx.log(ngx.DEBUG, string.format(fmt, ...))
 end
 
-local function infof(fmt, ...)
-    local c = debug.getinfo(2)
-    log.info("[etcd.lua:%d:%s] "..fmt, c.currentline, c.name, ...)
+function infof(fmt, ...)
+    ngx.log(ngx.INFO, string.format(fmt, ...))
 end
 
-local function errorf(fmt, ...)
-    local c = debug.getinfo(2)
-    log.error("[etcd.lua:%d:%s] "..fmt, c.currentline, c.name, ...)
+function errorf(fmt, ...)
+    ngx.log(ngx.ERR, string.format(fmt, ...))
 end
 
-function pp(...)
-    local values = {...}
-    for index, value in ipairs(values) do
-        print(json.encode(value))
-    end
+function base64_encode(str)
+    return ngx.encode_base64(str)
 end
 
-local function spawn(func)
-    local f = fiber.create(func)
-    f:name("etcd:"..uuid.str())
-    return f
-end
+function uri_parse(str)
+    local m = ngx.re.match(str, "((http|https)://)?([^/?]*)([^#]*)(.*)")
+    if m then
+        local login, pass
+        local host, port = m[3], nil
+        local path = m[4]
+        local anchor = m[5]
+        if path:sub(1, 1) ~= "/" then
+            path = "/" .. path
+        end
 
-function killall()
-    local fibers = fiber.info()
-    for fid, fvalue in pairs(fiber.info()) do
-        if string.sub(fvalue.name, 1, 5) == "etcd:" then
-            infof("kill fiber id=%d, name=%s", fid, fvalue.name)
-            local f = fiber.find(fid)
-            if f:status() ~= "dead" then
-                f:cancel()
+        m = ngx.re.match(host, "^([^@]+)@(.+)$")
+        if m then
+            login, host = m[1], m[2]
+        end
+
+        if login then
+            m = ngx.re.match(login, "^([^:]+):(.+)$")
+            if m then
+                login, pass = m[1], m[2]
             end
         end
-    end
-end
 
-function file_exists(snap_glob)
-    local files = fio.glob(snap_glob)
-    if (#files > 0) then
-        return true
-    end
-    return false
-end
-
-function call_timeout(timeout, func, ...)
-    local out = fiber.channel(2)
-    local args = {...}
-
-    function delay()
-        fiber.sleep(timeout)
-        out:put(nil)
-    end
-
-    function call()
-        local r = func(unpack(args))
-        out:put({r})
-    end
-
-    local f1 = fiber.create(delay)
-    local f2 = fiber.create(call)
-
-    local r = out:get()
-
-    if r == nil then
-        -- timeout
-        if f2:status() ~= "dead" then
-            f2:cancel()
+        m = ngx.re.match(host, "^([^:]*):([0-9]+)$")
+        if m then
+            host, port = m[1], m[2]
         end
 
-        return nil, true
-    end
-
-    -- success
-    if f1:status() ~= "dead" then
-        f1:cancel()
-    end
-
-    return r[1], false
+        return {
+            ["uri"] = str,
+            ["login"] = login,
+            ["password"] = pass,
+            ["host"] = host,
+            ["port"] = port,
+            ["path"] = path,
+            ["anchor"] = anchor,
+    }
+else
+    return nil
+end
 end
 
 function uri_escape(str)
-    local res = {}
-    if type(str) == 'table' then
-        for _, v in pairs(str) do
-            table.insert(res, uri_escape(v))
-        end
-    else
-        res = string.gsub(str, '[^a-zA-Z0-9_]',
-            function(c)
-                return string.format('%%%02X', string.byte(c))
-            end
-        )
-    end
-    return res
+    return ngx.escape_uri(str)
 end
 
 function uri_add_param(uri, param, value)
@@ -187,19 +143,19 @@ function Client.clone(self)
         user = self.user,
         password = self.password,
         timeout = self.timeout,
-    })
+        })
 end
 
 function Client.request(self, method, query, timeout)
     local headers = {}
 
-    local u = urilib.parse(self.addr)
+    local u = uri_parse(self.addr)
     if u["login"] ~= nil then
         local pass = u["password"]
         if pass == nil then
             pass = ""
         end
-        headers["Authorization"] = "Basic "..digest.base64_encode(u["login"]..":"..pass)
+        headers["Authorization"] = "Basic "..base64_encode(u["login"]..":"..pass)
     end
 
     local url = uri_join("http://", self.addr, "/v2/keys", query)
@@ -209,15 +165,14 @@ function Client.request(self, method, query, timeout)
     end
 
     debugf("%s %s (timeout=%d)", method, url, timeout)
+
+    local h = http.new()
+    h:set_timeout(timeout * 1000)
+
+    local r, err = h:request_uri(url, {method = method, headers = headers})
     
-    local r, is_timeout = call_timeout(timeout, http_client.request, method, url, "", {headers=headers})
-
-    if is_timeout then
-        r = {status  = 595, reason  = "timeout"}
-    end
-
     if r == nil then
-        r = {status  = 595, reason  = "unknown error"}
+        r = {status  = 595, reason  = err}
     end
 
     if r["headers"] ~= nil and r["headers"]["x-etcd-index"] ~= nil then
@@ -315,7 +270,7 @@ end
 -- </CLIENT>
 
 -- <DISCOVERY>
-function Client.announce(self, key, value, opts)
+function Client.announce(self, key, value, shm, opts)
     local client = self:clone()
 
     if opts == nil then
@@ -333,331 +288,48 @@ function Client.announce(self, key, value, opts)
     end
 
     function refresh()
-        local set_required = true
+        local schedule, think
+        local lock_key = key .. "_lock"
 
-        while true do
-            if set_required then
-                local r = client:set(key, value, {timeout=opts.timeout, ttl=opts.ttl})
-                if has_error(r) then
-                    errorf("announce %s set error: %s", key, error_message(r))
-                    fiber.sleep(1)
-                else
-                    set_required = false
-                    infof("announce %s set success", key)
-                    fiber.sleep(opts.refresh)
-                end
-            else -- !set_required
-                local r = client:refresh(key, {timeout=opts.timeout, ttl=opts.ttl})
-                if has_error(r) then
-                    errorf("announce %s refresh error: %s", key, error_message(r))
-                    set_required = true
-                else
-                    fiber.sleep(opts.refresh)
-                end
-            end -- if set_required
+        schedule = function(interval)
+            local ok, err = ngx.timer.at(interval, think)
+            if not ok then
+                etcd.errorf("failed to create timer: %s", err)
+            end
+        end
 
-        end -- while
+        think = function()
+            if not shm:add(lock_key, true, opts.refresh) then 
+                schedule(math.random(1, opts.refresh))
+                return
+            end
+
+            local r = client:refresh(key, {timeout=opts.timeout, ttl=opts.ttl})
+
+            local has_error = etcd.has_error(r)
+            if has_error then
+                etcd.errorf("announce %s refresh error: %s", key, etcd.error_message(r))
+
+                r = client:set(key, value, {timeout=opts.timeout, ttl=opts.ttl})
+                has_error = etcd.has_error(r)
+
+                if has_error then
+                    etcd.errorf("announce %s set error: %s", key, etcd.error_message(r))
+                end
+            end
+
+            if has_error then
+                shm:set(lock_key, true, 1)
+                schedule(1)
+            else
+                schedule(opts.refresh)
+            end
+        end
+
+        return schedule(0.1)
     end
 
-    return spawn(refresh)
+    return refresh()
 end
 
 -- </DISCOVERY>
-
--- <CLUSTER>
-
-local Cluster = {}
-
-function Client.cluster(self, key, self_addr, opts)
-    if opts == nil then
-        opts = {}
-    end
-
-    cluster = {
-        client = self:clone(),
-        key = key,
-        self_addr = self_addr,
-        replica_user = opts["replica_user"],
-        replica_password = opts["replica_password"],
-        discovery_ttl = opts["discovery_ttl"],
-        discovery_refresh = opts["discovery_refresh"],
-        role = opts["role"],
-        is_bootstrap = false,
-        bootstrap_tmp_uuid = "",
-        bootstrap_ttl = 30, -- maximum time between box.cfg{...} and cluster:run()
-        dump = opts["dump"],
-        data = {}, -- internal state. backuped to dump file
-    }
-
-    -- default values
-    if cluster.replica_user == nil then
-        cluster.replica_user = "guest"
-    end
-
-    if cluster.replica_password == nil then
-        cluster.replica_password = ""
-    end
-
-    if cluster.discovery_ttl == nil then
-        cluster.discovery_ttl = 60 -- one minute
-    end
-
-    if cluster.discovery_refresh == nil then
-        cluster.discovery_refresh = 10 -- every 10 seconds
-    end
-
-    if cluster.role == nil then
-        cluster.role = "master"
-    end
-
-    setmetatable(cluster, {__index = Cluster})
-
-    -- read data from dump file
-    cluster:restore()
-
-    return cluster
-end
-
-function Cluster.path(self, part)
-    return uri_join(self.key, part)
-end
-
-function Cluster.save(self) -- backup data to dump file
-    if self.dump == nil or self.dump == "" then
-        return
-    end
-
-    local dump_fn = fio.basename(self.dump)
-    local dump_dir = fio.dirname(self.dump)
-    local tmp_file = fio.pathjoin(dump_dir, "."..dump_fn)
-
-    local fh = fio.open(tmp_file, {"O_TRUNC", "O_WRONLY", "O_CREAT"})
-    if fh ~= nil then
-        if fh:write(json.encode(self.data)) then
-            if fh:fsync() and fh:close() then
-                fio.rename(tmp_file, self.dump)
-            end
-        end
-    else
-        errorf("open %s for write failed", tmp_file)
-    end 
-end
-
-function Cluster.restore(self) -- read data from dump file
-    if self.dump == nil or self.dump == "" then
-        return
-    end
-
-    local fh = fio.open(self.dump, {"O_RDONLY"})
-    if fh ~= nil then
-        -- file exists
-        local c = fh:read(1024*1024)
-        fh:close()
-
-        local data = json.decode(c)
-        if data ~= nil then
-            infof("restored from dump: %s", c)
-            self.data = data
-        end
-    else
-        errorf("restore from %s failed", self.dump)
-    end
-end
-
-function Cluster.replication_source_from_server(self)
-    while true do
-        local r = self.client:get(self:path("/master/"))
-        if has_error(r) then
-            errorf("fetch replication source from %s error: %s", self:path("/master/"), error_message(r))
-        else
-            local response = json.decode(r.body)
-            local sources = {}
-
-            if response["node"] ~= nil and response["node"]["nodes"] ~= nil then
-                for index,node in ipairs(response["node"]["nodes"]) do
-                    if node["value"] ~= nil then
-                        local value = json.decode(node["value"])
-                        if value ~= nil and value["addr"] ~= nil then
-                            table.insert(sources, value["addr"])
-                        end
-                    end
-                end
-            end
-
-            return sources
-        end
-
-        fiber.sleep(1)
-    end
-end
-
-function Cluster.bootstrap(self)
-    self.is_bootstrap = true
-    self.bootstrap_tmp_uuid = uuid.str()
-end
-
-function Cluster.bootstrap_replication_source(self)
-    -- Work:
-    -- 1. check "cluster_id" node
-    -- 2. if exists, then read masters list (break if not empty)
-    -- 3. if not exists, try to create
-    -- 4. go to 1.
-    local cluster_id = self:path("cluster_id")
-
-    while true do
-        infof("check %s", cluster_id)
-
-        local r = self.client:get(cluster_id)
-        
-        if r.status == 200 then
-            -- exists cluster_id, check for masters
-            infof("%s found", cluster_id)
-
-            local s, timeout = call_timeout(5, Cluster.replication_source_from_server, self)
-            if timeout then
-                errorf("fetch masters timeout")
-            elseif #s > 0 then
-                infof("master: %s", json.encode(s))
-                self.is_bootstrap = false -- no need confirm success start
-                return s
-            else
-                errorf("master list is empty")
-            end
-
-        elseif r.status == 404 and self.role == "master" then
-            -- not found cluster_id, try to create
-            infof("%s not found", cluster_id)
-
-            local r = self.client:create(cluster_id, self.bootstrap_tmp_uuid, {ttl=self.bootstrap_ttl})
-            if has_error(r) then
-                errorf("create %s failed: %s", cluster_id, error_message(r))
-            else
-                -- new cluster
-                infof("created %s: {value=%s, ttl=%d}", cluster_id, self.bootstrap_tmp_uuid, self.bootstrap_ttl)
-                return {}
-            end
-        else
-            errorf("check %s failed: %s", cluster_id, error_message(r))
-        end
-
-        fiber.sleep(1)
-    end
-end
-
-function Cluster.replication_source(self)
-    -- returns replication_sources for first box.cfg{}
-    if self.is_bootstrap then
-        local s = self:bootstrap_replication_source()
-        return self:replication_source_to_dsn(s)
-    end
-
-    local s, is_timeout = call_timeout(10, Cluster.replication_source_from_server, self)
-    if not is_timeout then
-        self:store_replication_source(s)
-    end
-
-    return self:replication_source_to_dsn(self.data["replication_source"])
-end
-
--- save new sources to self.data
--- dump to file
--- returns true if changed
-function Cluster.store_replication_source(self, sources)
-    infof("store_replication_source: %s", json.encode(sources))
-    
-    table.sort(sources)
-
-    if self.data["replication_source"] == nil then
-        self.data["replication_source"] = sources
-        self:save()
-        return true
-    end
-
-    if #sources ~= #self.data["replication_source"] then
-        self.data["replication_source"] = sources
-        self:save()
-        return true
-    end
-
-    for index, s in pairs(sources) do
-        if sources[index] ~= self.data["replication_source"][index] then
-            self.data["replication_source"] = sources
-            self:save()
-            return true
-        end
-    end
-
-    return false
-end
-
--- add user/password to host:port list
-function Cluster.replication_source_to_dsn(self, sources)
-    if sources == nil then
-        return {}
-    end
-
-    local dsn = {}
-
-    for index, s in ipairs(sources) do
-        if s ~= self.self_addr then
-            if self.replica_password ~= nil and self.replica_password ~= "" then
-                table.insert(dsn, self.replica_user..":"..self.replica_password.."@"..s)
-            else
-                table.insert(dsn, self.replica_user.."@"..s)
-            end
-        end
-    end
-
-    return dsn
-end
-
-function Cluster.run(self)
-    
-    if self.is_bootstrap then
-        -- finish bootstrap
-        local new_cluster_id = box.space._schema:select{'cluster'}[1][2]
-        while true do
-            local r = self.client:cas(self:path("cluster_id"), self.bootstrap_tmp_uuid, new_cluster_id)
-            if has_error(r) then
-                errorf("bootstrap finish failed: %s", error_message(r))
-                fiber.sleep(1)
-            else
-                break
-            end
-        end
-    end
-
-    -- recreate cluster_id if need (existing cluster moves to etcd)
-    local r = self.client:get(self:path("cluster_id"))
-    if r.status == 404 then
-        local cluster_id = box.space._schema:select{'cluster'}[1][2]
-        infof("cluster_id not found. set %s=%s", self:path("cluster_id"), cluster_id)
-        self.client:set(self:path("cluster_id"), cluster_id)
-    end
-    
-    -- register self discovery
-    self.client:announce(
-        self:path(uri_join(self.role, box.info.server.uuid)),
-        '{"addr": "'..self.self_addr..'"}',
-        {ttl = self.discovery_ttl, refresh = self.discovery_refresh}
-    )
-
-    function refresh_replication_source()
-        while true do
-            local sources = self:replication_source_from_server()
-            
-            if self:store_replication_source(sources) then
-                infof("new master: %s", json.encode(sources))
-                box.cfg{replication_source=self:replication_source_to_dsn(sources)}
-            end
-
-            self.client:wait(self:path("/master/"), {recursive=true, timeout=60})
-        end
-    end
-
-    spawn(refresh_replication_source)
-end
--- </CLUSTER>
-
-
-
